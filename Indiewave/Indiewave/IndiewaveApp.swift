a73505d0bf359f6b219ef7a9db37b33cda8d36b3
Richmond Aisabor <richmondaisabor@Richmonds-MacBook-Pro.local>
2021-06-27 17:06:56 -0500

    init commit

---
+++ Indiewave/Indiewave/IndiewaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndiewaveApp.swift
+//  Indiewave
+//
+//  Created by Richmond Aisabor on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct IndiewaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
