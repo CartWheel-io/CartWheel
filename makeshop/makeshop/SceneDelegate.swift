5bc020517ead800485fb754ebbdd54a74c7c5a2f
Richmond Aisabor <richmondaisabor@Richmonds-MacBook-Pro.local>
2021-11-05 15:40:57 -0500

    App exits user sign off

--- makeshop/makeshop/SceneDelegate.swift
+++ makeshop/makeshop/SceneDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -35,6 +37,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+       
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
@@ -45,12 +48,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        try? Auth.auth().signOut()
     }
 
 
